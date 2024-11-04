fcf7ea92777b3956d4173e0636aa398418470a1c
HzDev3628 <artem.khar5uk@gmail.com>
2024-11-04 15:38:33 +0200

    feat: add spline 3d

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import Spline from '@splinetool/react-spline/next'
+
 export default function Home() {
-	return <div className="font-bold">main page</div>;
+  return (
+    <div className="font-bold flex items-center justify-center">
+      main page
+      <div className="w-[600px] h-[440px] overflow-hidden">
+        <Spline
+          scene="https://prod.spline.design/eolxO7z91jjYft7U/scene.splinecode"
+          style={{ width: '700px', height: '500px', marginLeft: '-50px', marginTop: '-10px' }}
+        />
+      </div>
+    </div>
+  )
 }
