b9d7add085f230344f0ad29a3d6315da540de18c
HzDev3628 <artem.khar5uk@gmail.com>
2024-11-06 10:17:10 +0200

    delete: suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,19 @@
-import React, { Suspense } from 'react'
-
-const Spline = React.lazy(() => import('@splinetool/react-spline/next'))
+import Spline from '@splinetool/react-spline/next'
 
 export default function Home() {
   return (
     <div className="font-bold flex flex-col items-center justify-center">
       main page
       <div className="w-[600px] h-[440px] overflow-hidden">
-        <Suspense fallback={<div>Loading...</div>}>
-          <Spline
-            scene="https://prod.spline.design/eolxO7z91jjYft7U/scene.splinecode"
-            style={{
-              width: '800px',
-              height: '500px',
-              marginLeft: '-50px',
-              marginTop: '-10px',
-            }}
-          />
-        </Suspense>
+        <Spline
+          scene="https://prod.spline.design/eolxO7z91jjYft7U/scene.splinecode"
+          style={{
+            width: '800px',
+            height: '500px',
+            marginLeft: '-50px',
+            marginTop: '-10px',
+          }}
+        />
       </div>
     </div>
   )
