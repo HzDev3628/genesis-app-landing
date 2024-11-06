deda1a7dc0eb3cfb360f6c59e34a88dbd4a175a9
HzDev3628 <artem.khar5uk@gmail.com>
2024-11-06 10:13:19 +0200

    feat: icons, tailwind.config.ts, palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,28 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      screens: {
+        'xl': '1120px'
+      }
+    },
+    borderRadius: {
+      24: '24px',
+      12: '12px',
+      8: '8px',
+      6: '6px'
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        50: "var(--50)",
+        100: "var(--100)",
+        200: "var(--200)",
+        300: "var(--300)",
+        400: "var(--400)",
+        500: "var(--500)",
       },
     },
   },
