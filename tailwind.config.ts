13ea923761d74b3cfa456c0503c1aba8e10a8a60
HzDev3628 <artem.khar5uk@gmail.com>
2024-11-06 12:24:42 +0200

    fix: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,37 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
       center: true,
       screens: {
-        'xl': '1120px'
-      }
+        xl: '1120px',
+      },
     },
     borderRadius: {
-      24: '24px',
+      20: '20px',
+      16: '16px',
       12: '12px',
       8: '8px',
-      6: '6px'
     },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        50: "var(--50)",
-        100: "var(--100)",
-        200: "var(--200)",
-        300: "var(--300)",
-        400: "var(--400)",
-        500: "var(--500)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        50: 'var(--50)',
+        100: 'var(--100)',
+        200: 'var(--200)',
+        300: 'var(--300)',
+        400: 'var(--400)',
+        500: 'var(--500)',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
