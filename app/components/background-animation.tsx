b7a64688272f44d78edb1d885a40d96e0628f169
HzDev3628 <artem.khar5uk@gmail.com>
2024-11-09 16:56:47 +0200

    add: animation for cloud-2

--- app/components/background-animation.tsx
+++ app/components/background-animation.tsx
@@ -4,13 +4,23 @@ import Image from 'next/image'
 
 export const BackgroundAnimation = () => {
   return (
-    <div className="w-[3072px] h-[920px] absolute -z-10 top-0 animate-cloud ">
-      <Image
-        src={'/cloud-1.png'}
-        alt="Cloud"
-        fill
-        className="!w-[3072px] h-[920px]"
-      />
-    </div>
+    <>
+      <div className="w-[3072px] h-[920px] absolute -z-10 top-0 animate-cloud">
+        <Image
+          src={'/cloud-1.png'}
+          alt="Cloud"
+          fill
+          className="!w-[3072px] h-[920px]"
+        />
+      </div>
+      <div className="w-[3072px] h-[280px] absolute z-10 bottom-0 animate-cloud">
+        <Image
+          src={'/cloud-2.png'}
+          alt="Cloud"
+          fill
+          className="!w-[3072px] h-[280px]"
+        />
+      </div>
+    </>
   )
 }
