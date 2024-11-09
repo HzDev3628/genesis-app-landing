f303c19f533774f62f9611d549f4e857f3a2e654
HzDev3628 <artem.khar5uk@gmail.com>
2024-11-09 18:38:50 +0200

    fix: select-none

--- app/components/background-animation.tsx
+++ app/components/background-animation.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export const BackgroundAnimation = () => {
   return (
     <>
-      <div className="w-[3072px] h-[920px] absolute -z-10 right-0 top-0 animate-cloud">
+      <div className="w-[3072px] h-[920px] absolute -z-10 right-0 top-0 animate-cloud select-none">
         <Image
           src={'/cloud-1.png'}
           alt="Cloud"
@@ -13,7 +13,7 @@ export const BackgroundAnimation = () => {
           className="!w-[3072px] h-[920px]"
         />
       </div>
-      <div className="w-[3072px] h-[280px] absolute z-10 right-0 bottom-0 animate-cloud">
+      <div className="w-[3072px] h-[280px] absolute z-10 right-0 bottom-0 animate-cloud select-none">
         <Image
           src={'/cloud-2.png'}
           alt="Cloud"
